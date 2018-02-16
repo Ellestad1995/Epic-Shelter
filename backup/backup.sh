7c2e41fcbb6cd7b191a3fc0781b7964d26c66171
Stoned <stoned@T460p>
2018-02-16 14:51:30 +0100

    Backup script som kopierer mysqldump over til backupVM

---
+++ backup/backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+######### BEFORE RUNNING #########
+#eval`ssh-agent`
+#ssh-add id_rsa
+######### BEFORE RUNNING #########
+
+TMP=$(ls /home/ubuntu/backups)
+DIRSTAT=$?
+
+if [[ DIRSTAT -ne 0 ]]; then
+  mkdir /home/ubuntu/backups
+fi
+
+
+
+#Lage en dump fil med riktig navn
+sudo mysqldump --opt --master-data=2 --flush-logs \
+--all-databases > /home/ubuntu/backups/backup.$(date +%d_%m_%y_%R).sql
+
+LATESTMODFILE=$(ls -1 -t /home/ubuntu/backups/ | head -1)
+
+#Sende alt til backupvmen --> scp med privatekey
+scp -r -v -o StrictHostKeyChecking=no /home/ubuntu/backups/$LATESTMODFILE ubuntu@10.10.1.78:/home/ubuntu/backups
