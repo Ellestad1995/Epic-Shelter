0ef9f0187ffef9a5964c692f6dfde1b1f19e134e
Joakim Ellestad <joakine@stud.ntnu.no>
2018-02-18 12:33:48 +0100

    Added haproxy file

---
+++ manager/autonomous_haproxy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+#Made by Joakim Ellestad, joakine@stud.ntnu.no
+
+
+#When edit are made to the servers holding www files haproxy needs to be updated with correct ports and
+#ip addresses and names
